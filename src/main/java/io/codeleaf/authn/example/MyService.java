d378ae929f3d3c4f1da14bf89da7be66c035a1de
Deepak Shingan <deepak.shingan@gmail.com>
2019-01-24 11:01:24 +0530

    fixed issues. Working with password authentication

--- src/main/java/io/codeleaf/authn/example/MyService.java
+++ src/main/java/io/codeleaf/authn/example/MyService.java
@@ -3,13 +3,20 @@ package io.codeleaf.authn.example;
 import io.codeleaf.authn.AuthenticationContext;
 import io.codeleaf.authn.NotAuthenticatedException;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
+@Path("")
+@Produces("application/json")
+@Consumes("application/json")
 public class MyService {
+
     @GET
+    @Path("/auth")
     public String getHelloWorld() {
         try {
-            System.out.println("OK........");
             return AuthenticationContext.isAuthenticated() ?
                     AuthenticationContext.get().getIdentity() : "I am good!";
         } catch (NotAuthenticatedException e) {
