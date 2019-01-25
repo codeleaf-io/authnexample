0599ade090099913e4f4de9f5fabda559fff43b0
Tom Burger <tvburger@Thomass-MacBook-Air.local>
2019-01-25 20:58:27 +0100

    Updates dependencies

--- src/main/java/io/codeleaf/example/MyApplication.java
+++ src/main/java/io/codeleaf/example/MyApplication.java
@@ -1,11 +1,17 @@
-package io.codeleaf.authn.example;
+package io.codeleaf.example;
 
 import io.codeleaf.authn.jaxrs.spi.JaxrsAuthenticationFilterFactory;
+import io.codeleaf.logging.core.LogBindings;
+
 import javax.ws.rs.core.Application;
 import java.util.HashSet;
 import java.util.Set;
 
 public class MyApplication extends Application {
+
+    static {
+        LogBindings.init();
+    }
 
     public java.util.Set<java.lang.Object> getSingletons() {
         Set<java.lang.Object> set = new HashSet<>();
--- src/main/java/io/codeleaf/example/MyService.java
+++ src/main/java/io/codeleaf/example/MyService.java
@@ -1,7 +1,9 @@
-package io.codeleaf.authn.example;
+package io.codeleaf.example;
 
 import io.codeleaf.authn.AuthenticationContext;
 import io.codeleaf.authn.NotAuthenticatedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -13,10 +15,15 @@ import javax.ws.rs.Produces;
 @Consumes("application/json")
 public class MyService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MyService.class);
+
     @GET
     @Path("/auth")
     public String getHelloWorld() {
         try {
+            System.out.println("hello console out!");
+            System.err.println("hello console err!");
+            LOGGER.info("Are we authenticated? " + AuthenticationContext.isAuthenticated());
             return AuthenticationContext.isAuthenticated() ?
                     AuthenticationContext.get().getIdentity() : "I am good!";
         } catch (NotAuthenticatedException e) {
